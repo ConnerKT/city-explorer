0995813e1dfb54a78a5fbedfff50b6c924e64d6e
ConnerKT <connerthompson121@gmail.com>
2023-05-02 19:27:44 -0500

    Update Form.js

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,10 +1,26 @@
 import Form from "react-bootstrap/Form"
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { useState } from 'react';
+
+
 
 function CityForm() {
+    const [state, setState] = useState({
+        searchQuery:'',
+        location:{}
+    });
+
+    const getLocation = async () => {
+    let apiGet = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${state.searchQuery}&format=json`
+    let res = await axios.get(apiGet);
+    console.log(res.data[0])
+    }
+
    function handleSubmit(event){
     event.preventDefault();
-    fetch('https://us1.locationiq.com/v1/search.php')
+    getLocation();
+    
 
    }
     return(
@@ -12,7 +28,7 @@ function CityForm() {
         <Form onSubmit={handleSubmit}>
             <Form.Group>
                 <Form.Label>City Name</Form.Label>
-                <Form.Control type="text" placeholder="Type Here..."/>
+                <Form.Control value={setState.searchQuery} type="text" placeholder="Type Here..."/>
                 <Button variant="primary" type="submit">Explore!</Button>
             </Form.Group>
         </Form>
